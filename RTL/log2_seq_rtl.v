7206d96c1de9e476ad23268c39172bd080dd22f7
Mohammad Huda Ansari <23bec147@sot.pdpu.ac.in>
2026-04-07 16:14:45 +0530

    Change Y_comb assignment to use X_comb

--- RTL/log2_seq_rtl.v
+++ RTL/log2_seq_rtl.v
@@ -12,14 +12,14 @@ always @(posedge clk) begin
     X_comb <= X;
 end
 always @(*) begin
-    if (X[7])       Y_comb = 3'd7;
-    else if (X[6])  Y_comb = 3'd6;
-    else if (X[5])  Y_comb = 3'd5;
-    else if (X[4])  Y_comb = 3'd4;
-    else if (X[3])  Y_comb = 3'd3;
-    else if (X[2])  Y_comb = 3'd2;
-    else if (X[1])  Y_comb = 3'd1;
-    else if (X[0])  Y_comb = 3'd0;
+    if (X_comb[7])       Y_comb = 3'd7;
+    else if (X_comb[6])  Y_comb = 3'd6;
+    else if (X_comb[5])  Y_comb = 3'd5;
+    else if (X_comb[4])  Y_comb = 3'd4;
+    else if (X_comb[3])  Y_comb = 3'd3;
+    else if (X_comb[2])  Y_comb = 3'd2;
+    else if (X_comb[1])  Y_comb = 3'd1;
+    else if (X_comb[0])  Y_comb = 3'd0;
     else            Y_comb = 3'd0;
 end
 
