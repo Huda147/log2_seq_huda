ee0b585c86de6f1866440d9aa0941e7a927815fa
Mohammad Huda Ansari <23bec147@sot.pdpu.ac.in>
2026-04-07 16:16:20 +0530

    Update log2_seq_rtl.v

--- RTL_SIMULATION/log2_seq_rtl.v
+++ RTL_SIMULATION/log2_seq_rtl.v
@@ -5,12 +5,14 @@ module log2_seq (
     input [7:0] X,
     output reg [2:0] Y
 );
-reg [7:0] X_comb
+
+reg [7:0] X_comb;
 reg [2:0] Y_comb;
-    
+
 always @(posedge clk) begin
     X_comb <= X;
 end
+
 always @(*) begin
     if (X_comb[7])       Y_comb = 3'd7;
     else if (X_comb[6])  Y_comb = 3'd6;
@@ -20,7 +22,7 @@ always @(*) begin
     else if (X_comb[2])  Y_comb = 3'd2;
     else if (X_comb[1])  Y_comb = 3'd1;
     else if (X_comb[0])  Y_comb = 3'd0;
-    else            Y_comb = 3'd0;
+    else                Y_comb = 3'd0; // X = 0 case
 end
 
 always @(posedge clk) begin
